f57efedc0b9853b63bba75c6233ff9579e615302
Kallinteris Andreas <kallinteris@protonmail.com>
2024-05-23 09:55:06 +0300

    update

---
+++ humanoid_ctrl_cost_action/mujoco_old/__init__.py
@@ -0,0 +1,15 @@
+#from mujoco_old import MujocoEnv, MuJocoPyEnv  # isort:skip
+
+# ^^^^^ so that user gets the correct error
+# message if mujoco is not installed correctly
+#from .ant_v5 import AntEnv
+##from .half_cheetah_v5 import HalfCheetahEnv
+#from .hopper_v5 import HopperEnv
+from .humanoid_v5 import HumanoidEnv
+from .humanoidstandup_v5 import HumanoidStandupEnv
+#from .inverted_double_pendulum_v5 import InvertedDoublePendulumEnv
+#from .inverted_pendulum_v5 import InvertedPendulumEnv
+#from .pusher_v5 import PusherEnv
+#from .reacher_v5 import ReacherEnv
+#from .swimmer_v5 import SwimmerEnv
+#from .walker2d_v5 import Walker2dEnv
